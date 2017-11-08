589ce86d20c37b067362f7b93d9d83ae233ac4e3
Simon Coulombe <simoncoulombe@gmail.com>
2017-11-08 00:43:39 -0500

    réparé l'encodage avec iconv()

--- emailSAM.R
+++ emailSAM.R
@@ -9,15 +9,26 @@ library(sf)
 library(leaflet)
 library(htmlwidgets)
 register_google(key = Sys.getenv("googlemap_api_key"))
+#https://stackoverflow.com/questions/33068063/encoding-and-raw-in-r
+#https://stackoverflow.com/questions/27690036/losing-the-%C4%8C-%C4%8D-characters-utf-encoding-within-r
+#https://stackoverflow.com/questions/6254744/override-a-function-that-is-imported-in-a-namespace
+  my_base64url_decode_to_char <- function(x) { iconv(rawToChar(base64enc::base64decode(gsub("_", "/", gsub("-", "+", x)))), from="UTF-8", to="latin1") }
+# # 
+ getAnywhere("base64url_decode_to_char")
+ unlockBinding("base64url_decode_to_char", getNamespace("gmailr"))
+ assign("base64url_decode_to_char", my_base64url_decode_to_char, getNamespace("gmailr"))
+
+  
+
 #this works
 # send_message(mime(from="simoncoulombe@gmail.com", to="simoncoulombe@gmail.com",
 #                   subject="hello", "how are you doing?"))
 
 
 my_messages = messages("from=noreply@samweb.com", num_results=1)
-my_message = message(id = my_messages[[1]]$messages[[1]]$id, format="full")
 
-maisons$adresse
+my_message = message(id = my_messages[[1]]$messages[[1]]$id, format="full")
+body(my_message)
 
 z <- body(my_message)  %>%  str_extract_all("UNI.+\\(vente\\)")
 urls <- body(my_message)  %>%  str_extract_all('https://ng.samweb.com.+?"')
@@ -27,10 +38,6 @@ adresse <- str_replace(z[[1]], ".+?-\ ", "")  %>% #? pour rendre le + lazy au li
   str_replace("Sainte-Foy/Sillery/Cap-Rouge", "") %>% 
   str_replace("Saint-Louis", "") %>% 
   str_replace("Sillery", "") %>% 
-  str_replace("Ã©", "é") %>% ## ça ça ne marche pas trop.. j'ai lancé une issue pour l'encodage
-  str_replace("Ã", "à") %>%
-  str_replace("Ã´", "ô")  %>%
-  str_replace("Les Chutes-de-la-Chaudià¨re-Est", "")  %>%
   str_replace("Les Chutes-de-la-Chaudière-Est", "")  %>%
   str_replace("Saint-Romuald", "")  
   
@@ -60,8 +67,10 @@ map_data <-  st_as_sf(maisons %>% arrange(prix) %>%
                       coords = c("lon", "lat"), 
                       crs = 4326, agr = "constant") 
 
+map_data <- map_data %>% mutate(label = paste0(prix, " - " ,adresse))
+
 my_map <- map_data %>% leaflet() %>% 
-  addCircles(label= ~ paste0(prix, " - " ,adresse),
+  addCircles(label= ~ label,
              #popup= '<a href = "https://rstudio.github.io/leaflet/"> R </a>') %>%
               #popup= ~paste0('<a href = "https://rstudio.github.io/leaflet/"> R </a>')) %>%
              #popup= ~paste0('<a href = ',"https://rstudio.github.io/leaflet/",'> R </a>')) %>%
@@ -70,3 +79,4 @@ my_map <- map_data %>% leaflet() %>%
 
 my_map
 saveWidget(my_map, file="emailSAM.html")
+
